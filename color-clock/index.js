b9c32b34ee1d3147d601bd1fa7244c9014f14b2d
Isabella Costabile <costabileisa@gmail.com>
2022-12-14 21:59:38 -0500

    commit tree 801b1503c7861fa64d7ae68ca60d665169aa1e92

--- color-clock/index.js
+++ color-clock/index.js
@@ -7,9 +7,9 @@ function displayTime() {
   clock.textContent = format(new Date(), "MMMM do yyyy, h:mm:ss a");
   setInterval(function () {
     clock.textContent = format(new Date(), "MMMM do yyyy, h:mm:ss a");
-    clock.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    clock.style.backgroundColor =
-      "#" + Math.floor(Math.random() * 16777215).toString(16);
+  //  clock.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  //  clock.style.backgroundColor =
+  //    "#" + Math.floor(Math.random() * 16777215).toString(16);
   }, 1000);
 }
 
